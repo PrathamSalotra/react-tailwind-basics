9065c952cca58a0d832e8a2b7f72864cd118e63e
PrathamSalotra <salotrapratham28@gmail.com>
2026-07-24 19:55:08 +0530

    card created for right section

--- src/components/Section1/Pagecontentsection2.jsx
+++ src/components/Section1/Pagecontentsection2.jsx
@@ -3,8 +3,9 @@ import Rightcard from './Rightcard'
 
 const Pagecontentsection2 = () => {
   return (
-    <div className='h-full w-3/4 p-3 bg-blue-400'>
+    <div className='h-full w-3/4 px-10 flex gap-5'>
         <Rightcard/>
+        
     </div>
   )
 }
--- src/components/Section1/Rightcard.jsx
+++ src/components/Section1/Rightcard.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import {ArrowRight} from 'lucide-react'
 const Rightcard = () => {
   return (
-    <div className='h-full w-1/3 bg-amber-200 rounded-3xl  overflow-hidden relative'>
-        <img className='h-full w-full object-cover' src="https://plus.unsplash.com/premium_photo-1706727289010-4bf8e72c9143?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+    <div className='h-full w-1/3 bg-amber-200 rounded-3xl  overflow-hidden relative '>
+        <img className='h-full w-full object-cover' src="https://plus.unsplash.com/premium_photo-1661630621969-6d9faac03f9f?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
 
-        <div className='absolute top-0 left-0 w-full h-full bg-amber-200 p-7 flex flex-col items-start'>
+        <div className='absolute top-0 left-0 w-full h-full p-7 flex flex-col items-start justify-between'>
             <h1 className='bg-white px-5 py-3 rounded-full font-bold'>1</h1>
-            <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur perferendis doloremque sed repellat architecto?</div>
-            <div>
-                <button><ArrowRight /></button>
+            <div className='text-xl flex flex-col gap-15 text-white'>
+                <p >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur perferendis doloremque sed repellat architecto?</p>
+                <button className='flex items-center gap-3 bg-blue-500 rounded-full py-2 px-21'>Satisfied<ArrowRight /></button>
             </div>
         </div>
     </div>
