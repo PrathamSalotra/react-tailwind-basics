74951044f3e19a48243eb725e9937e9542a0f056
PrathamSalotra <salotrapratham28@gmail.com>
2026-07-25 16:14:36 +0530

    removing the scrollbar for the right section

--- src/components/Section1/Pagecontentsection2.jsx
+++ src/components/Section1/Pagecontentsection2.jsx
@@ -4,7 +4,7 @@ import Rightcard from './Rightcard'
 const Pagecontentsection2 = (props) => {
     let count = 0;
   return (
-    <div className='h-full w-3/4 px-10 flex gap-5 overflow-x-auto'>
+    <div id='rightsection' className='h-full w-3/4 px-10 flex gap-5 overflow-x-auto'>
         {props.users.map((elements)=>{
             count++;
             return <Rightcard img={elements.image} desc={elements.description} tag={elements.tag} count={count}/>
